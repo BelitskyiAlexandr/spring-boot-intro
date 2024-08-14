3c02ab137cb3929ddbce5ee8c98dbdd65e4faae0
BelitskyiAlexandr <vilisapett@gmail.com>
2024-08-14 18:14:00 +0300

    checkstyle

--- src/main/java/com/example/bookshop/model/Book.java
+++ src/main/java/com/example/bookshop/model/Book.java
@@ -1,7 +1,10 @@
 package com.example.bookshop.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
