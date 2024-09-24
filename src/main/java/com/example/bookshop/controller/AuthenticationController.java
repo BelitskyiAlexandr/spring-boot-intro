a72b0e5587456c208c3e17d3e073ee00db6f8434
BelitskyiAlexandr <vilisapett@gmail.com>
2024-09-24 21:53:06 +0300

    Olena's fixes

--- src/main/java/com/example/bookshop/controller/AuthenticationController.java
+++ src/main/java/com/example/bookshop/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import com.example.bookshop.dto.user.UserRegistrationRequestDto;
 import com.example.bookshop.dto.user.UserResponseDto;
 import com.example.bookshop.security.AuthenticationService;
 import com.example.bookshop.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +21,13 @@ public class AuthenticationController {
     private final UserService userService;
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "registration", description = "Allow user to register in the system")
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.register(requestDto);
     }
 
+    @Operation(summary = "login", description = "Allow user to authenticate to the system")
     @PostMapping("/login")
     public UserLoginResponseDto login(
             @RequestBody @Valid UserLoginRequestDto userLoginRequestDto) {
