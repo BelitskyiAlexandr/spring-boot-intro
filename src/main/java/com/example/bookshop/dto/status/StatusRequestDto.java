172cbfbd5ea404279a3bf891c59573ba6056c8e8
BelitskyiAlexandr <vilisapett@gmail.com>
2025-08-11 19:31:13 +0300

    model + dto

---
+++ src/main/java/com/example/bookshop/dto/status/StatusRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.bookshop.dto.status;
+
+import com.example.bookshop.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StatusRequestDto {
+    private Status status;
+}
